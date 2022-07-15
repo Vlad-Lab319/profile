df99f798b8dabb5517573d6ecd4fbfe116654b5e
vlad-lab319 <solo15mag@gmail.com>
2022-07-15 14:56:09 -0400

    Add projects carousel

--- src/components/Websites.js
+++ src/components/Websites.js
@@ -1,10 +1,42 @@
 import './Websites.scss';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
-function Websites () {
+function Websites() {
+
+  const pictures = [
+    '3dtoronto',
+    'aphrodite',
+    'clarion',
+    'ewheel',
+    'webrtc',
+  ];
+
+  const carousel = pictures.map(async (pic, i) => {
+    <div>
+      <img src={await require(`../assets/img/screenshots/Screenshot_${pic}.webp`)} alt="pic" />
+      <p className='carousel-legend'>test {i}</p>
+    </div>
+  });
+
+  console.log(carousel);
+
+  const testCarousel = pictures.map((pic, i) => {
+    return (
+        <div>
+          <img src={require(`../assets/img/screenshots/Screenshot_${pic}.webp`)} alt="3Dtoronto WebBP" />
+          <p className='carousel-legend'>{pic}</p>
+        </div>
+    )
+
+  })
 
   return (
-    <div>
+    <div className='projects'>
       Websites
+      <Carousel>
+        {testCarousel}
+      </Carousel>
     </div>
 
   );
