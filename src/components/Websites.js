a5750a4da7c27709daaa380c4d93e24c80bbaaab
vlad-lab319 <solo15mag@gmail.com>
2022-07-15 20:49:44 -0400

    Add resized jpg to test Amplify

--- src/components/Websites.js
+++ src/components/Websites.js
@@ -19,26 +19,50 @@ function Websites() {
     'webrtc',
   ];
 
+  const pics = [
+
+  ]
+
   const testCarousel = pictures.map((pic, i) => {
     return (
       <div>
-        <Image name={pic}/>
+        <Image name={pic} />
         <p className='carousel-legend'>{pic}</p>
       </div>
     )
 
   })
 
+  const carousel =
+    <div>
+      <Carousel>
+        {/* <img src={require("../assets/img/screenshots/Screenshot_3dtoronto.webp")} alt="" className='carousel-img'/>
+        <img src={require("../assets/img/screenshots/Screenshot_clarion.webp")} alt=""className='carousel-img'/>
+        <img src={require("../assets/img/screenshots/Screenshot_aphrodite.webp")} alt="" className='carousel-img'/>
+        <img src={require("../assets/img/screenshots/Screenshot_ewheel.webp")} alt=""className='carousel-img'/>
+        <img src={require("../assets/img/screenshots/Screenshot_webrtc.webp")} alt=""className='carousel-img'/> */}
+        <img src={require("../assets/img/jpg/Screenshot_3dtoronto.jpg")} alt="" className='carousel-img'/>
+        <img src={require("../assets/img/jpg/Screenshot_clarion.jpg")} alt=""className='carousel-img'/>
+        <img src={require("../assets/img/jpg/Screenshot_aphrodite.jpg")} alt="" className='carousel-img'/>
+        <img src={require("../assets/img/jpg/Screenshot_ewheel.jpg")} alt=""className='carousel-img'/>
+        <img src={require("../assets/img/jpg/Screenshot_webrtc.jpg")} alt=""className='carousel-img'/>
+      </Carousel>
+    </div>
+
+
+
+
   return (
     <div className='projects'>
-      Websites
+      {/* Websites */}
       {/* <Image name={'3dtoronto'}/> */}
 
-      <Carousel
+      {/* <Carousel
         showArrows={true}
       >
         {testCarousel}
-      </Carousel>
+      </Carousel> */}
+      {carousel}
     </div>
 
   );
