897b8be8e67820d95a0c5afe304bc7ff57a4c74e
vlad-lab319 <solo15mag@gmail.com>
2022-08-16 14:51:53 -0400

    Add responsive design for contacts page

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,9 +2,50 @@ import './Contacts.scss';
 import Social from './Social';
 import Canvas from './Canvas';
 import Overbracket from './Overbracket';
+import useWindowDimensions from '../hooks/useWindowDimensions';
+
 
 function Contacts() {
 
+  const { height, width } = useWindowDimensions();
+
+  let wh;
+  let ww = {};
+
+  
+  if (width < 450) {
+    ww['website'] = 180;
+    ww['linkedin'] = 160; 
+    ww['twitter'] = 160; 
+    ww['instagram'] = 180; 
+    ww['email'] = 320; 
+    
+  } else if (width < 800) {
+    ww['website'] = 380;
+    ww['linkedin'] = 320; 
+    ww['twitter'] = 360; 
+    ww['instagram'] = 380; 
+    ww['email'] = 720; 
+    
+  } else {
+    ww['website'] = 380;
+    ww['linkedin'] = 320; 
+    ww['twitter'] = 360; 
+    ww['instagram'] = 380; 
+    ww['email'] = 720; 
+  }
+  
+  if (height < 450) {
+    wh=40
+    ww['website'] = 260;
+    ww['linkedin'] = 240; 
+    ww['twitter'] = 240; 
+    ww['instagram'] = 240; 
+    ww['email'] = 480; 
+
+  } else {
+    wh=60
+  }
 
   return (
     <div className='contacts'>
@@ -12,13 +53,13 @@ function Contacts() {
         <p className='canvas-text'>
           Website
         </p>
-        <Overbracket width={380} height={60} />
+        <Overbracket width={ww.website} height={wh} />
       </div>
       <div className='canvas linkedin'>
         <p className='canvas-text'>
           LinkedIn
         </p>
-        <Overbracket width={320} height={60} />
+        <Overbracket width={ww.linkedin} height={wh} />
       </div>
 
 
@@ -53,19 +94,19 @@ function Contacts() {
       </span>
 
       <div className='canvas twitter'>
-        <Canvas width={340} height={60} />
+        <Canvas width={ww.twitter} height={wh} />
         <p className='canvas-text'>
           Twitter
         </p>
       </div>
       <div className='canvas insta'>
-        <Canvas width={380} height={60} />
+        <Canvas width={ww.instagram} height={wh} />
         <p className='canvas-text'>
           Instagram
         </p>
       </div>
       <div className='canvas e-mail'>
-        <Canvas width={680} height={60} />
+        <Canvas width={ww.email} height={wh} />
         <p className='canvas-text'>
           E-mail
         </p>
