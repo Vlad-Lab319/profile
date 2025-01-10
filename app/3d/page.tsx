805abe0fa1448dc7b43dec844e72aa20fe2b756c
vlad-lab319 <solo15mag@gmail.com>
2025-01-10 12:00:38 -0500

    fix missing module error

--- app/3d/page.tsx
+++ app/3d/page.tsx
@@ -3,16 +3,10 @@ import React from "react";
 console.log("React version:", React.version);
 import dynamic from "next/dynamic";
 
-// Dynamically import 3d model
-const ModelViewer = dynamic(() => import("../components/models/ModelViewer"), {
-  ssr: false,
-});
-
 const Page3D = () => {
   return (
     <div>
       <h1>3D Model</h1>
-      <ModelViewer />
     </div>
   );
 };
